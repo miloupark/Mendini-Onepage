c3f59c0ee9b6034fca31f3f368a83e101d98ff4d
miloupark <136055426+miloupark@users.noreply.github.com>
2023-10-24 16:52:07 +0900

    수정중

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,6 +1,24 @@
 AOS.init();
 gsap.registerPlugin(ScrollTrigger);
 
+
+
+// new fullpage('#fullpage', {
+//   // options here
+// });
+
+
+$(document).ready(function() {  
+  //activate fullpage.js
+  $('#fullpage').fullpage({
+    scrollBar: true,
+    navigation: true,
+    navigationTooltips: ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4'],
+    loopBottom: true,
+    sectionSelector: 'section'
+  });
+});
+
 // header-title_alessandro
 gsap.to(".title-l-clip", {
   y: -100, // 슬라이딩할 거리 설정
@@ -49,80 +67,10 @@ var swiper = new Swiper(".mySwiper-object1", {
 var swiper = new Swiper(".mySwiper-object2", {
   slidesPerView: 6,
   spaceBetween: 30,
-  // direction: 'reversed',
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
 });
   
-// $(document).ready(function() {
-// 	$('#fullpage').fullpage({
-// 		//options here
-// 		autoScrolling:true,
-// 		scrollHorizontally: true
-// 	});
- 
-// 	//methods
-// 	$.fn.fullpage.setAllowScrolling(false);
-// });
 
-// if (첫번째섹션이라면) { 	
-// 	$("img").attr("data-aos", "fade-down") 	
-// 	aos.refreshHard() } 
-// else if (첫번째 섹션이 아니라면) {
-//  	$("#section1 [data-aos]").attr("data-aos", "") 	
-// aos.refreshHard() 
-// }
-
-
-
-// afterLoad : function(origin, destination) {
-//   $('#fullpage .section').find('.animated').each(function() {
-//           var $this = $(this);
-//           var Ani = $this.attr('data-animated');
-//           $this.removeClass(Ani);
-//         });
-//         $('#fullpage .section').eq(destination - 1).find('.animated').each(function () {
-//           var $this = $(this);
-//           var Ani = $this.attr('data-animated');
-//           $this.addClass(Ani);
-//         });
-//   }
-
-// // gsap.registerPlugin(ScrollTrigger);
-// gsap.to(".mendini-who-shape-circle", {
-//   scrollTrigger: {
-//     trigger: ".mendini-who",
-//     markers: true,
-//     start: "20% 1px",
-// 		scrub: true,
-//   },
-//   x: -300, duration: 1,
-// });
-
-// const headerEl = document.querySelector('#header');
-
-// // 페이지에 스크롤 이벤트 추가 
-// window.addEventListener(
-//   'scroll',
-//   // _.throttle(함수, 시간)
-//   // 스크롤이 300ms마다 한번씩 실행되도록 설정
-//   _.throttle(function(){
-//     console.log(window.scrollY);
-//     // 페이지의 스크롤 위치가 200px보다 크면
-//     if (window.scrollY > 200) {
-//       // gsap.to(요소, 시간, 옵션);
-//       gsap.to(headerEl, 0.6, {
-//         opacity: 0,
-//         display: 'none',
-//       });
-//     } else {
-//         // 페이지의 스크롤 위치가 200px보다 작으면
-//         gsap.to(headerEl, 0.6, {
-//           opacity: 1,
-//           display: 'block',
-//         });
-//       }
-//   }, 300)
-// );
