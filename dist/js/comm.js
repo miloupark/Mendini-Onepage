5941011ab642b06dc707dae6f4d902a89fbacef3
miloupark <136055426+miloupark@users.noreply.github.com>
2023-10-31 14:53:26 +0900

    수정중

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,16 +1,3 @@
-AOS.init();
-gsap.registerPlugin(ScrollTrigger);
-
-
-
-
-// object card
-var swiper = new Swiper(".mySwiper", {
-  effect: "cards",
-  grabCursor: true,
-});
-
-
 // matter.js of fullpage 1st section
 var w = window.innerWidth;
 var h = window.innerHeight;
@@ -128,77 +115,35 @@ Matter.World.add(engine.world, mouseConstraint);
 Matter.Engine.run(engine);
 
 
-
-
-
-// new fullpage('#fullpage', {
-//   // options here
+// // object card
+// var swiper = new Swiper(".mySwiper1", {
+//   effect: "cards",
+//   grabCursor: true,
+// });
+// var swiper = new Swiper(".mySwiper2", {
+//   effect: "cards",
+//   grabCursor: true,
+// });
+// var swiper = new Swiper(".mySwiper3", {
+//   effect: "cards",
+//   grabCursor: true,
 // });
 
-
-// $(document).ready(function() {  
-//   //activate fullpage.js
-//   $('#fullpage').fullpage({
-//     scrollBar: true,
-//     navigation: true,
-//     navigationTooltips: ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4'],
-//     loopBottom: true,
-//     sectionSelector: 'section'
-//   });
-// });
-
-// // header-title_alessandro
-// gsap.to(".title-l-clip", {
-//   y: -100, // 슬라이딩할 거리 설정
-//   duration: 1, // 애니메이션 지속 시간 설정
-//   repeat: -1, // 무한 반복
-//   ease: "linear" // 선형 이징 사용
-// });
-
-
-// // Mendini who - mendini name
-// var ctrl = new ScrollMagic.Controller();
-
-// $(".mendini-who").each(function(i) {
-//   var inner = $(this).find(".amuleto-outer");
-//   var outer = $(this).find(".amuleto-inner");
-//   var tl = new TimelineMax();
-  
-//   tl.from(outer, 0.25, { scaleX: 0 });
-//   tl.from(inner, 0.65, { yPercent: 100, ease: Back.easeOut });
-  
-//   new ScrollMagic.Scene({
-//     triggerElement: this,
-//     triggerHook: 0,
-//   })
-//     .setTween(tl)
-//     .addIndicators({
-//       colorTrigger: "white",
-//       colorStart: "white",
-//       colorEnd: "white",
-//       indent: 40
-//     })
-//     .addTo(ctrl);
-// });
-
-
-// // Object-Swiper
-// var swiper = new Swiper(".mySwiper-object1", {
-//   slidesPerView: 6,
-//   spaceBetween: 30,
-//   autoplay: {
-//     delay: 2500,
-//     disableOnInteraction: false,
-//   },
-// });
-// // Object-Swiper2
-// var swiper = new Swiper(".mySwiper-object2", {
-//   slidesPerView: 6,
-//   spaceBetween: 30,
-//   autoplay: {
-//     delay: 2500,
-//     disableOnInteraction: false,
-//   },
-// });
-  
-
+// Object-Swiper
+var swiper = new Swiper(".mySwiper-object1", {
+  slidesPerView: 6,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+// Object-Swiper2
+var swiper = new Swiper(".mySwiper-object2", {
+  slidesPerView: 6,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
